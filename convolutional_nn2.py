fea6f8faa9dd5726992e716d867518efaa36861c
Alexander Wyrstiuk <awyrstiuk@gmail.com>
2025-06-30 19:01:37 -0400

    Create convolutional_nn2.py

---
+++ convolutional_nn2.py
@@ -0,0 +1,2 @@
+#Work for this project was directly used in the McGill course ECSE 551, and the professor has requested that I not post the code related to this project.
+#This would otherwise contain the code for the second convolutional neural net that I tried.
