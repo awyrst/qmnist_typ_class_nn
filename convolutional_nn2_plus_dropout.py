dc5adf6d80e0ab6f403489effd82798357ad13ba
Alexander Wyrstiuk <awyrstiuk@gmail.com>
2025-06-30 18:58:55 -0400

    Update convolutional_nn2_plus_dropout.py

--- convolutional_nn2_plus_dropout.py
+++ convolutional_nn2_plus_dropout.py
@@ -1,2 +1,2 @@
 #Work for this project was directly used in the McGill course ECSE 551, and the professor has requested that I not post the code related to this project.
-#This would otherwise contain the code for the second convolutional neural net that I tried, with the 50% dropout added.
+#This would otherwise contain the code for the second convolutional neural net that I tried, with the 50% dropout added at select layers.
